e10e0ca9ff879dda9b033b79120b6e560367286f
m-satao-19 <satao19.madhura@gmail.com>
2018-09-21 00:25:18 +0530

    new student profile not yet completed logout done

--- src/main/java/com/pict/LogoutServlet.java
+++ src/main/java/com/pict/LogoutServlet.java
@@ -20,10 +20,39 @@ public class LogoutServlet extends HttpServlet {
 
     public LogoutServlet () {
         super();
-        databaseConnection = DatabaseConnection.getDatabaseConnection();
+        //databaseConnection = DatabaseConnection.getDatabaseConnection();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+            String login_mis_id=request.getParameter("login_mis_id");
+            String login_pswd=request.getParameter("login_pswd");
+
+            Connection con;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mentorsys", "hello", "hello");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        out.println ("SL3 "+ "database successfully opened.");
+            out.print(login_mis_id);
+            out.print(login_pswd);
+
+        HttpSession session=request.getSession();
+        session.removeAttribute("stud_name");
+        session.invalidate();
+        response.sendRedirect("../index.jsp");
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session=request.getSession();
         session.removeAttribute("stud_name");
         session.invalidate();
--- src/main/webapp/jsp/student_profile.jsp
+++ src/main/webapp/jsp/student_profile.jsp
@@ -129,7 +129,7 @@
             response.sendRedirect("/index.jsp");
         String stud_name = (String)session.getAttribute("stud_name");
         String stud_prn = (String)session.getAttribute("stud_prn");%>
-    <a class="active" href="LogoutServlet">LogOut</a>
+    <a class="active" href="../LogoutServlet">LogOut</a>
     <a href="#"><%=stud_name%></a>
 </div>
 <div class="container-fluid">
