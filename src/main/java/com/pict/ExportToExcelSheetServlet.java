009105768b97d01f8957392648e0db406c3cf65d
srushti98 <srushtipawar12@gmail.com>
2018-10-19 19:21:26 +0530

    whole project with admin and mentor and mom meeting backend with excel import and with new admin index page and students side and triggers and murumkar's approval with search bar with uploading backend and solving sweetalert error of admin with loading button and upload certificates validation and with alert problem in students side

--- src/main/java/com/pict/ExportToExcelSheetServlet.java
+++ src/main/java/com/pict/ExportToExcelSheetServlet.java
@@ -1,24 +1,19 @@
 package com.pict;
 
-import com.pict.database.DatabaseConnection;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.*;
-
-import  org.apache.poi.hssf.usermodel.HSSFSheet;
-import  org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import  org.apache.poi.hssf.usermodel.HSSFRow;
-import  org.apache.poi.hssf.usermodel.HSSFCell;
 
 import static java.lang.System.out;
 
@@ -28,12 +23,13 @@ public class ExportToExcelSheetServlet extends HttpServlet {
 
     private Connection databaseconnection;
 
-    private ExportToExcelSheetServlet(){
-        super();
-        databaseconnection= DatabaseConnection.getDatabaseConnection();
-    }
+//    private ExportToExcelSheetServlet(){
+//        super();
+//        databaseconnection= DatabaseConnection.getDatabaseConnection();
+//    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        out.print("in servlet!!!!!!!!!!!");
         try {
             Connection con;
             Class.forName("com.mysql.jdbc.Driver");
@@ -42,6 +38,7 @@ public class ExportToExcelSheetServlet extends HttpServlet {
             PreparedStatement ps = null;
             ps=con.prepareStatement("select * from studentmentorrel");
 
+            out.println("\n\n\n\n");
             out.println("1");
             ResultSet rs=ps.executeQuery();
 
@@ -72,6 +69,8 @@ public class ExportToExcelSheetServlet extends HttpServlet {
             FileOutputStream out1=new FileOutputStream(new File("Student_mentor_information.xls"));
 
             workbook.write(out1);
+
+
             out1.close();
 
             out.println("File created successfully");
@@ -92,6 +91,63 @@ public class ExportToExcelSheetServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        out.print("in servlet!!!!!!!!!!!");
+        try {
+            Connection con;
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mentorsys", "hello", "hello");
+            out.println("export " + "database successfully opened.");
+            PreparedStatement ps = null;
+            ps=con.prepareStatement("select * from studentmentorrel");
 
+            out.println("\n\n\n\n");
+            out.println("1");
+            ResultSet rs=ps.executeQuery();
+
+            HSSFWorkbook workbook=new HSSFWorkbook();
+            out.println("2");
+            HSSFSheet sheet=workbook.createSheet("Old_Mentor_student");
+            out.println("3");
+            HSSFRow row=sheet.createRow(1);
+
+            out.println("4");
+            HSSFCell cell;
+            out.println("5");
+            cell=row.createCell(0);
+            cell.setCellValue("Mentor ID");
+            cell=row.createCell(1);
+            cell.setCellValue("MIS ID");
+
+            int i=2;
+            row = sheet.createRow(i);
+            while(rs.next()){
+                cell = row.createCell(0);
+                cell.setCellValue(rs.getString("emp_id"));
+                cell = row.createCell(1);
+                cell.setCellValue(rs.getString("stud_mis_id"));
+                row=sheet.createRow(++i);
+            }
+
+            FileOutputStream out1=new FileOutputStream(new File("information.xls"));
+            System.out.print(out1.toString());
+
+            workbook.write(out1);
+            out1.close();
+
+            out.println("File created successfully");
+
+            con.close();
+
+            response.setContentType("application/vnd.ms-excel");
+            response.sendRedirect("/jsp/admin_index.jsp");
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            out.println(e);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            out.println(e);
+        }
     }
 }
